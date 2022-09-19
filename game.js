1b11b8c2bec229dc001288d4859e2869d268255b
João Suzuki <joaosuzuki98@outlook.com>
2022-09-19 11:40:27 -0300

    Adicionado o cronometro, os bonzinhos e  o renascimento dos invasores

--- game.js
+++ game.js
@@ -29,20 +29,71 @@ const posicElement = (el) => {
 // Desloca os elementos na tela
 // Recebe parâmetros elemento, velocidade , incremento
 const moveElemento = (el, veloc, inc) => {
-    
     //Executa a cada x milissegundos
     const anima = setInterval(() => {
+
         veloc = veloc + inc
         el.style.left = veloc + 'px'
 
-        // Verifica se elemento saiu do quadro e possui a
-        // classe emtela
-        // Remove a classe e volta para uma posição inicial
-        if(veloc > larguraQuadro) {
+        // Verifica se elemento saiu do quadro ou se foi clicado (classe "morto")
+        // retorna para uma posição à esquerda do quadro (re-entra)
+        if (veloc > larguraQuadro || el.classList.contains('morto')) {
+            // Sorteia um valor entre
+            veloc = -Math.random() * 450 + 50
+            inc = Math.random() * 40 + 10
             posicElement(el)
+            el.classList.remove('morto')
         }
+        // Adiciona atributo velocidade para consulta no código JS
+        el.setAttribute('velocidade', inc)
     }, 40)
-} 
+}
+
+// Ao clicar nos insetos
+const clickBug = (el) => {
+    console.log(el.getAttribute('id'))
+
+    // // Adiciona uma classe com o nome de 'morto'
+    el.classList.add('morto')
+
+    // // Adiciona 10 pts ao score
+    score += 10
+
+    // Se o inseto clicado for "bonzinho" perde 50 pontos
+    if (el.classList.contains('bonzinho')) {
+        score -= 60
+    }
+
+    document.getElementById('score').innerText = score
+
+    // Se velocidade for maior que 20, faz 100 pontos
+    if (el.getAttribute('velocidade') > 20 && el.classList.contains('invasor')) {
+        score += 100
+
+        // Mostra +100 pontos após 1/2 segundo
+        // apenas nos insetos que tenham a classe "invasor"
+        let pts100 = document.getElementById('pts100')
+
+        pts100.style.left = el.style.left
+        pts100.style.top = el.style.top
+
+        /*const mostra100pts = setInterval(() => {
+            pts100.style.left = '-300px'
+
+            // Interrompe o setInterval
+            clearInterval(mostra100pts)
+        }, 500); */
+
+        // Executa o código após 500 milissegundos
+        const mostra10pts = setTimeout(() => {
+            pts100.style.left = '-300px'
+        }, 500)
+    }
+}
+
+
+
+
 
 
 /* 
@@ -51,7 +102,33 @@ const moveElemento = (el, veloc, inc) => {
 --------------------------------------------------
  */
 
-for(inv of invasores) {
+for (inv of invasores) {
     posicElement(inv)
-    moveElemento(inv, Math.random()*10, Math.random()*19+1)
+    moveElemento(inv, Math.random() * 10, Math.random() * 19 + 1)
+
+    // e.target = elemento que executa o evento, ou seja, inseto clicado
+    inv.addEventListener('mousedown', (e) => { clickBug(e.target) })
 }
+
+for (bom of bonzinhos) {
+    posicElement(bom)
+    moveElemento(bom, Math.random() * 10, Math.random() * 19 + 1)
+
+    // e.target = elemento que executa o evento, ou seja, inseto clicado
+    bom.addEventListener('mousedown', (e) => { clickBug(e.target) })
+}
+
+// Contagem regressiva
+setTimeout(() => {
+    // Avisa ao usuário o fim do tempo
+    alert('TEMPO ESGOTADO!!!')
+
+    // Recarrega a página - semelhante a F5
+    location.reload()
+}, tempoRestante * 1000 + 2000);
+
+const mostraTempo = setInterval(() => {
+    // A cada segundo mostra o tempo restante
+    document.getElementById('infoTR').innerText = tempoRestante
+    document.getElementById('temporest').innerText = tempoRestante--
+}, 1000);
